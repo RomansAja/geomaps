22c2d9a38b09bb40af990514bbd2c71916b9eb63
RomansAja <143486982+RomansAja@users.noreply.github.com>
2025-01-08 20:14:37 +0700

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -27,7 +27,20 @@ return [
     | Supported Drivers: "local", "ftp", "sftp", "s3"
     |
     */
-
+    'local' => [
+        'driver' => 'local',
+        'root' => storage_path('app'),
+        'permissions' => [
+            'file' => [
+                'public' => 0644,
+                'private' => 0600,
+            ],
+            'dir' => [
+                'public' => 0755,
+                'private' => 0700,
+            ],
+        ],
+    ],
     'disks' => [
 
         'local' => [
